2d968493fb47ca04a0996a4314a49baec5d8715b
DeviousMalcontent <deviousmalcontent@gmail.com>
2022-12-11 01:15:38 +1100

    Updated day01.ps1
    The first problem solved, now on to Part 2, there is no commenting on this code because I will do that later when I refactor. - this is probably a lie...

--- day01/day01.ps1
+++ day01/day01.ps1
@@ -1,13 +1,21 @@
 $thisElfNumber, $totalCalories = 0;
+$arrayThatHoldsTheTotalCaloriesForEachElf = New-Object System.Collections.Generic.List[System.Object]
 [string[]]$arrayFromFile = Get-Content -Path 'input'
 $thisElfNumber = $thisElfNumber+1
 for ($i=0; $i -lt $arrayFromFile.Length; $i++) {
 	if ($arrayFromFile[$i] -ne "") {
 		[int]$totalCalories = [int]$totalCalories + [int]$arrayFromFile[$i] 
+
 	} else {
 		echo "Elf $thisElfNumber is carrying: $totalCalories Calories."
 		$thisElfNumber++
+		$arrayThatHoldsTheTotalCaloriesForEachElf.Add($totalCalories);
 		$totalCalories = 0;
 	}
 }
-#echo $arrayFromFile
+echo "------------------------------------------"
+#$arrayThatHoldsTheTotalCaloriesForEachElf.ToArray() | out-null
+$arrayThatHoldsTheTotalCaloriesForEachElf.ToArray() | sort
+#for ($k=0; $k -lt $arrayThatHoldsTheTotalCaloriesForEachElf.Length; $k++) {
+#	$arrayThatHoldsTheTotalCaloriesForEachElf[$k]
+#}
